78ee7e9bb41ccbb67a39b4dff453e6c996206fe3
atletisimo <37110896+atletisimo@users.noreply.github.com>
2022-08-15 18:28:13 +0200

    Update DataTable.js

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -1,8 +1,10 @@
 import React from "react";
 
-const DataTable = ({ vrednost }) => {
+const DataTable = ({ vrednost,netoWeek,netoMonth,tax }) => {
   return (
     <div className="table">
+      {netoWeek}
+      {netoMonth}
       <table class="table-auto bg-gray-900 relative bottom-48  w-11/12  object-left">
         <div class=" text-white ">
           {vrednost} your net{" "}
@@ -17,17 +19,32 @@ const DataTable = ({ vrednost }) => {
         <tr>
           <th class="bg-red-700">Frequency </th>
           <th class="bg-red-700">Gross income</th>
-          <th class="bg-red-700">Tax</th>
+          <th class="bg-red-700">Tax {tax}%</th>
           <th class="bg-red-700">Net income </th>
         </tr>
+       
+        <tr class="text-white">Weekly
+        <td></td>
+        <td>{tax} %</td>
+        </tr>
+        <tr class="text-white">Fortnightly
+        <td></td>
+        <td>{tax} %</td>
         
-        <tr class="text-white">Weekly</tr>
-        <tr class="text-white">Fortnightly</tr>
-        <tr class="text-white">Monthly</tr>
-        <tr class="text-white">Anually</tr>
+        </tr>
+        <tr class="text-white">Monthly
+        <td></td>
+        <td>{tax} %</td>
+        </tr>
+        <tr class="text-white">Anually
+        <td></td>
+        <td>{tax} %</td>
+        </tr>
+        
       </table>
     </div>
   );
 };
 
 export default DataTable;
+
